67314b381dc3ddf51498b126eedf11f0f8f032dd
wenhao <wh_wang@live.com>
2017-11-19 01:20:46 +0800

    change to gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get 'courses/index'
 
+  #root 'courses#index'
+  #to change the root
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
