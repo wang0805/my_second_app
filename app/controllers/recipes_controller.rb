74b4941112a7a7dfa05195380cb88aaa77dfefcf
wenhao <wh_wang@live.com>
2017-11-19 02:31:34 +0800

    adding images

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
-  	@searh_term1 = 'Chicken'
+  	@searh_term1 = params[:looking_for] || 'Chicken'
   	@recipes = Recipes.for(@searh_term1)
   end
 end
